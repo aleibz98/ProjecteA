b7294d6363b2d5c95cc65252a23dd45ab8358f3c
aleibz98 <alejandro.alarcon.t@est.fib.upc.edu>
2020-10-18 15:51:37 +0200

    Implementació d'alguns testers.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,26 @@ using namespace std;
 
 typedef Grafo bool[][];
 
+struct Datos_generador{
+    int tamaño;     //Tamany del graf
+    float q;        //coeficient q
+    float comps;    //nombre de components connexes que s'han generat
+
+    void print() {
+        cout << tamaño << ' ' << q << ' ' << comps << endl;
+    }
+};
+
+struct Datos_perc_aristas{
+    int tamaño;
+    float q;
+    float prob_connex;
+
+    void print() {
+        cout << tamaño << ' ' << q << ' ' << prob_connex << endl;
+    }
+};
+
 list< list > matrixToList(Grafo g){
     list< list<int> > result;
     for(int i = 0; i < g.size(); i++){
@@ -26,6 +46,8 @@ int contarCompConexas(Grafo g){
     bool checks[g.size()] = {false};
     queue<int> q;
     q.push(0);
+
+    //BFS
     while(nodosVisitados < g.size()){
         componentes++;
         while(not q.empty()){
@@ -40,6 +62,7 @@ int contarCompConexas(Grafo g){
                 }
             }
         }
+
         for(int i = 0; i < g.size() and q.empty(); ++i){
             if(not checks[i]) q.push(i);
         }
@@ -61,7 +84,7 @@ Grafo generador_basico(int size, float probabilidad){
 }
 
 Grafo percolacion_nodos(list< list<int> > g, float q){
-
+    //Aqui es suposa que el graf estaria representat amb la llista d'adjacencies
 
 }
 
@@ -85,9 +108,11 @@ bool graellaValida(Grafo g){
     bool[g.size()][g.size()] checks = {false};
 
     for(int i = 0; i < g.size(); i++){
-        s.push(make_pair(i,g.size()-1));
+        s.push(make_pair(i,g.size()-1));    //Primer fiquem tota la fila d'abaix a la pila
     }
-    while(not s.empty() and not result){
+
+    //DFS
+    while(not s.empty()){
         pair<int,int> actual = s.top();
         s.pop();
         checks[actual.first][actual.second] = true;
@@ -116,7 +141,52 @@ bool graellaValida(Grafo g){
 
 
 int main() {
-
     srand (time(NULL));
+
+    //TESTERS
+    //Components connexes
+    int sizes[3] = {20, 60, 100};
+    Datos_generador res_connexio[300]; //struct per guardar la info
+    for(int k = 0; k < sizes.size(); k++) {
+        float coef = 0.0;
+        for (int i = 0; i < 100; ++i) {
+            int comps = 0;
+            for (int j = 0; j < 5; ++j) {
+                Grafo g = generador_basico(sizes[k],coef);
+                comps = contarCompConexas(g);
+            }
+            res_connexio[k*100 + i].comps = comps/5.0;
+            res_connexio[k*100 + i].q = coef;
+            res_connexio[k*100 + i].tamaño = sizes[k];
+
+            coef += 0.01;
+        }
+    }
+
+    //Percolacio arestes
+    //Suposarem que els grafs tenen que ser connexos
+    Datos_perc_aristas results[300];          //first tindra el valor del coeficient q i second tindra el valor de f(q)
+    for(int k = 0; k < sizes.size(); ++k) {
+        float val = 0.0;
+        for (int i = 0; i < 100; ++i) {            //Farem 20 iteracions buscant aproximar els punts de transició, per als quals f(q) passa de 0 -> 1
+            int suma = 0;
+            for (int j = 0; j < 10; j++) {        //Per a cada iteració, farem 10 proves per obtenir el promig
+                Grafo g = generador_basico(sizes[k],probabilitat);      //Aquesta probabilitat tindria que generar un graf connex
+                if(contarCompConexas(g) > 1) j--;
+                else {
+                    Grafo f = percolacion_aristas(g, val);
+                    if(contarCompConexas(f) == 1) suma += 1;
+                }
+            }
+            results[k*20 + i].tamaño = sizes[k];
+            results[k*20 + i].prob_connex = suma/10.0;
+            results[k*20 + i].q = val;
+
+            val += 0.01;
+        }
+    }
+
+
+
 
 }
